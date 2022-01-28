a9bbe8ffc88295d68b79c4a078887eb43cf8dee1
Zoe Rowbotham <zoe2.rowbotham@live.uwe.ac.uk>
2022-01-28 20:30:31 +0000

    Dimensional Object Script

---
+++ Assets/Scripts/DimensionalObject.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DimensionalObject : MonoBehaviour
+{
+    [SerializeField] private bool m_inCreepyDimension = false;
+
+    private void OnEnable()
+    {
+        DimensionSwitcher.OnDimensionSwitched += OnDimensionSwitched;
+    }
+
+    private void OnDisable()
+    {
+        DimensionSwitcher.OnDimensionSwitched -= OnDimensionSwitched;
+    }
+
+    private void OnDimensionSwitched(bool inCreepyDimension)
+    {
+        gameObject.SetActive(m_inCreepyDimension == inCreepyDimension);
+    }
+}
