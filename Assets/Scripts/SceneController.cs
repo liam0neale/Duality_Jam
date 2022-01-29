647851745859fc83959074200990db2719a27d97
liam0neale <liam0neale@gmail.com>
2022-01-29 10:55:40 +0000

    sceneManager API

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController
+{
+    const int LEVEL_1_INDEX = 1;
+    static int CurrentSceneIndex = 0;
+    
+    public static void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+	}
+    public static void LoadWinMenu()
+    {
+        SceneManager.LoadScene("WinMenu");
+        CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public static void LoadDeathMenu()
+    {
+        SceneManager.LoadScene("DeathMenu");
+        CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    public static void ReloadScene()
+    {
+        SceneManager.LoadScene(CurrentSceneIndex);
+    }
+    public static void LoadNextLevel()
+    {
+        CurrentSceneIndex++;
+        if (IndexInRange() == false)
+            return;
+        
+        SceneManager.LoadScene(CurrentSceneIndex);
+    }
+    public static void LoadLevel(int _levelIndex)
+    {
+        CurrentSceneIndex = _levelIndex;
+        if (IndexInRange() == false)
+            return;
+        SceneManager.LoadScene(CurrentSceneIndex);
+    }
+
+    static bool IndexInRange()
+    {
+        if (CurrentSceneIndex >= SceneManager.sceneCount)
+        {
+            Debug.LogError("SceneController::IndexInRange() -> index is out of range : index = " + CurrentSceneIndex.ToString());
+            return false;
+        }
+        return true;
+    }
+}
